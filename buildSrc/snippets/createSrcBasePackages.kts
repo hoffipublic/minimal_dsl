f2d79880e16c8b4129be41f26a9e3ba762fcbbf1
Dirk Hoffmann <hoffi.mail@gmail.com>
2023-02-16 16:44:58 +0100

    updated buildSrc

---
+++ buildSrc/snippets/createSrcBasePackages.kts
@@ -0,0 +1,52 @@
+/** create package dirs under each subprojects src/module/kotlin
+ * based on subproject's extra property: projectPackage
+ *
+ * in rootProject:
+ * //=============
+ * group = "com.hoffi"
+ * version = "1.0-SNAPSHOT"
+ * val artifactName by extra { "${rootProject.name.toLowerCase()}-${project.name.toLowerCase()}" }
+ * val rootPackage by extra { "${rootProject.group}.${rootProject.name.toLowerCase()}" }
+ * val theMainClass by extra { "Main" }
+ * application {
+ *     mainClass.set("${rootPackage}.${theMainClass}" + "Kt") // + "Kt" if fun main is outside a class
+ * }
+ *
+ * in subprojects:
+ * //=============
+ * group = "${rootProject.group}"
+ * version = "${rootProject.version}"
+ * val artifactName by extra { "${rootProject.name.toLowerCase()}-${project.name.toLowerCase()}" }
+ * val rootPackage: String by rootProject.extra
+ * val projectPackage by extra { "${rootPackage}.${project.name.toLowerCase()}" }
+ * val theMainClass by extra { "Main" }
+ * application {
+ *     mainClass.set("${projectPackage}.${theMainClass}" + "Kt") // + "Kt" if fun main is outside a class
+ * }
+ */
+tasks.register("createSrcBasePackages") {
+    doLast {
+        project.subprojects.forEach { sub ->
+            val projectPackage: String by sub.extra
+            val projectPackageDirString = projectPackage.split('.').joinToString("/")
+            sub.pluginManager.let() { when {
+                it.hasPlugin("org.jetbrains.kotlin.jvm") -> {
+                    sub.sourceSets.forEach { ss: SourceSet ->
+                        val ssDir = File("${sub.name}/src/${ss.name}/kotlin")
+                        if (ssDir.exists()) {
+                            mkdir("$ssDir/$projectPackageDirString")
+                        }
+                    }
+                }
+                it.hasPlugin("org.jetbrains.kotlin.multiplatform") -> {
+                    sub.kotlin.sourceSets.forEach { ss: org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet ->
+                        val ssDir = File("${sub.name}/src/${ss.name}/kotlin")
+                        if (ssDir.exists()) {
+                            mkdir("$ssDir/$projectPackageDirString")
+                        }
+                    }
+                }
+            }}
+        }
+    }
+}
--- buildSrc/src/main/kotlin/DepVersions.kt
+++ buildSrc/src/main/kotlin/DepVersions.kt
@@ -31,8 +31,8 @@ object DepVersions {
         Dep.from("com.github.ajalt.clikt:clikt:3.5.1", GROUP_CONSOLE),
 
         Dep.from("com.h2database:h2:2.1.214", GROUP_DB),
-        //Dep.from("org.postgresql:postgresql:42.5.2", GROUP_DB, versionRegex = "${THREEDIGITSs}\\.jre\\d*\$"),
-        Dep.from("org.postgresql:postgresql:42.5.2", GROUP_DB),
+        //Dep.from("org.postgresql:postgresql:42.5.3", GROUP_DB, versionRegex = "${THREEDIGITSs}\\.jre\\d*\$"),
+        Dep.from("org.postgresql:postgresql:42.5.3", GROUP_DB),
 
         // __LATEST_COMPOSE_RELEASE_VERSION__ https://github.com/JetBrains/compose-jb/releases
         Dep.from("org.jetbrains.compose:compose-gradle-plugin:1.3.0", GROUP_JETBRAINS, repo = JETBRAINSREPO),
@@ -67,7 +67,7 @@ object DepVersions {
         Dep.from("org.jetbrains.kotlin:kotlin-test-common:${BuildSrcGlobal.VersionKotlin}"),
         Dep.from("org.jetbrains.kotlin:kotlin-test-junit:${BuildSrcGlobal.VersionKotlin}"),
         Dep.from("org.jetbrains.kotlin:kotlin-test-annotations-common:${BuildSrcGlobal.VersionKotlin}"),
-        Dep.from("io.kotest:kotest-runner-junit5:5.5.4", GROUP_TESTING),
+        Dep.from("io.kotest:kotest-runner-junit5:5.5.5", GROUP_TESTING),
         Dep.from("org.hamcrest:hamcrest-library:2.2", GROUP_TESTING),
 
         Dep.from("com.rabbitmq:amqp-client:5.16.0", GROUP_WEB),
@@ -83,9 +83,9 @@ object DepVersions {
         return "$groupAndArtifact:${dep.version}"
     }
     fun versionOf(groupAndArtifact: String, otherDep: String): String {
-        val otherDep = vMap[otherDep] ?: throw GradleException("unknown dependency version (not in 'buildSrc/src/main/kotlin/DepVersions.kt'): \"$otherDep\"")
-        USED.getOrPut(otherDep.groupkey){emptySet<Dep>().toMutableSet()}.add(otherDep)
-        return "$groupAndArtifact:${otherDep.version}"
+        val theOtherDep = vMap[otherDep] ?: throw GradleException("unknown dependency version (not in 'buildSrc/src/main/kotlin/DepVersions.kt'): \"$otherDep\"")
+        USED.getOrPut(theOtherDep.groupkey){emptySet<Dep>().toMutableSet()}.add(theOtherDep)
+        return "$groupAndArtifact:${theOtherDep.version}"
     }
     fun versionOnly(groupAndArtifact: String): String {
         val dep = vMap[groupAndArtifact] ?: throw GradleException("unknown dependency version (not in 'buildSrc/src/main/kotlin/DepVersions.kt'): \"$groupAndArtifact\"")
